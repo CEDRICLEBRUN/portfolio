212cf2d2a9f187fe96fa2e8e01d46637cfcc7f8b
cedric le brun <lebruncedric8@gmail.com>
2022-12-02 14:13:21 +0100

    init of work component

---
+++ src/data/works.js
@@ -0,0 +1,17 @@
+import RidingPot from '../assets/projects/RidingPot.jpeg';
+import SB from '../assets/projects/SB.jpeg';
+
+export const works=[
+  {
+      id:1,
+      name:"Riding Pot",
+      image:RidingPot,
+      github:"https://github.com/clementsprs/riding-pot"
+  },
+  {
+    id:2,
+    name:"Sailing Bets",
+    image:SB,
+    github:"https://github.com/CEDRICLEBRUN/SAILING_BETS"
+}
+]
